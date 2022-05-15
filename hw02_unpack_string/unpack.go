f7d1e652dd8d159b98eac035f59dda2adbba5168
Tiksy <gvomelchenko@gmail.com>
2022-05-16 01:19:36 +0300

    HW2 is completed

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,38 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strconv"
+	"strings"
+	"unicode"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(a string) (string, error) {
+	var str strings.Builder
+	b := []rune(a)
+	switch {
+	case len(b) == 0: // check if len=0
+		return "", nil
+	case unicode.IsDigit(b[0]): // check if 1st sign is digit
+		return "", ErrInvalidString
+	}
+	// check if 2 digits side by side
+	for i := 0; i < len(b)-1; i++ {
+		if unicode.IsDigit(b[i]) && unicode.IsDigit(b[i+1]) {
+			return "", ErrInvalidString
+		}
+	}
+	for idx, val := range b {
+		if !unicode.IsDigit(val) {
+			str.WriteString(string(val))
+		} else {
+			tmpAnsw := str.String()
+			str.Reset()
+			str.WriteString(tmpAnsw[:len(tmpAnsw)-1])
+			strRepStep, _ := strconv.Atoi(string(val))
+			str.WriteString(strings.Repeat(string(b[idx-1]), strRepStep))
+		}
+	}
+	return str.String(), nil
 }
--- hw02_unpack_string/unpack_test.go
+++ hw02_unpack_string/unpack_test.go
@@ -16,6 +16,12 @@ func TestUnpack(t *testing.T) {
 		{input: "abccd", expected: "abccd"},
 		{input: "", expected: ""},
 		{input: "aaa0b", expected: "aab"},
+		// my positive tests
+		{input: "a4b*2d3e", expected: "aaaab**ddde"},
+		{input: "*4bc2d2e", expected: "****bccdde"},
+		{input: "a0b0c0", expected: ""},
+		{input: "a\t3b0c0d", expected: "a			d"},
+		{input: "ab 4cd", expected: "ab    cd"},
 		// uncomment if task with asterisk completed
 		// {input: `qwe\4\5`, expected: `qwe45`},
 		// {input: `qwe\45`, expected: `qwe44444`},
@@ -34,7 +40,7 @@ func TestUnpack(t *testing.T) {
 }
 
 func TestUnpackInvalidString(t *testing.T) {
-	invalidStrings := []string{"3abc", "45", "aaa10b"}
+	invalidStrings := []string{"3abc", "45", "aaa10b", "ab01cd"}
 	for _, tc := range invalidStrings {
 		tc := tc
 		t.Run(tc, func(t *testing.T) {
